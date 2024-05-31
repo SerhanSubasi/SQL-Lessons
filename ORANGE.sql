abcfb419370d7f09cc2ca1dbcb51fc7f97e2d954
Serhan Subaşı <142296842+SerhanSubasi@users.noreply.github.com>
2024-05-31 23:03:38 +0300

    7-6

--- ORANGE.sql
+++ ORANGE.sql
@@ -265,7 +265,60 @@ WHERE maas = 2500
 
 -- TEACHERS tablosu oluşturalım
 
+CREATE TABLE teachers(
+id int,
+firstname varchar(50),
+lastname varchar(50),
+age int,	
+city varchar(20),
+course_name varchar(20),
+salary real	
+);
 
+INSERT INTO teachers VALUES(111,'AhmeT  ','  Han',35,'Istanbul','SpringMVC',5000);
+INSERT INTO teachers VALUES(112,'Mehmet','Ran ',33,'Van','HTML',4000);
+INSERT INTO teachers VALUES(113,' Bilal','Fan ',34,'Bursa','SQL',3000);
+INSERT INTO teachers VALUES(114,'Celal',' San',30,'Bursa','Java',3000);
+INSERT INTO teachers VALUES(115,'Deniz',' Can',30,'Istanbul','SQL',3500);
+INSERT INTO teachers VALUES(116,'ekreM','Demir',36,'Istanbul','Java',4000.5);
+INSERT INTO teachers VALUES(117,'Fatma','Celik',38,'Van','SpringBOOT',5550);
+INSERT INTO teachers VALUES(118,'Hakan','Cetin',44,'Izmir','Java',3999.5);
+INSERT INTO teachers VALUES(119,'mert','Cetin',32,'Izmir','HTML',2999.5);
+INSERT INTO teachers VALUES(120,'Nilay','Cetin',32,'Izmir','CSS',2999.5);
+INSERT INTO teachers VALUES(121,'Selma','Celik',40,'Ankara','SpringBOOT',5550);
+INSERT INTO teachers VALUES(122,'fatiH','Can',32,'Ankara','HTML',2550.22);
+INSERT INTO teachers VALUES(123,'Nihat','Keskin',32,'Izmir','CSS',3000.5);
+INSERT INTO teachers VALUES(124,'Hasan','Temel',37,'Istanbul','S.Security',3000.5);
+
+SELECT * FROM teachers;
+
+-- SORU 1: 
+--teachers tablosundaki tüm kayıtların firstname değerlerini büyük harfe,
+--lastname değerlerini küçük harfe çevirerek,uzunlukları ile birlikte listeleyiniz.
+
+SELECT
+	UPPER(firstname) AS upper_firstname,
+	LENGTH(firstname) AS len_firstname,
+	LOWER(lastname) AS lower_lastname,
+	LENGTH(lastname) AS len_lastname
+FROM teachers t 
+
+
+-- SORU 2:
+--teachers tablosunda firstname ve lastname değerlerindeki başlangıç ve sondaki boşlukları kaldırınız.
+
+SELECT 
+	TRIM(firstname) AS clean_firstname , 
+	TRIM(lastname) AS clean_lastname
+FROM teachers t 
+
+-- SORU 3:
+--teachers tablosunda tüm kayıtların firstname değerlerini ilk harfini büyük diğerleri küçük harfe çevirerek görüntüleyiniz.
+
+SELECT 
+	firstname,
+	initcap(firstname) AS baslangic_buyuk_firstname 
+FROM teachers t 
 
 
 
